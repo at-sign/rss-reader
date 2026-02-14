159b6be858d8264b8e31d44a770e371bf8e101f1
at-sign <47769987+at-sign@users.noreply.github.com>
2026-02-13 23:19:23 -0600

    updated article links

--- src/template.js
+++ src/template.js
@@ -18,8 +18,6 @@ const article = (item) => `
         }
         <li class="article-links-item"><a href="https://archive.today/${item.link}" target='_blank' rel='noopener norefferer nofollow'>[a.to]</a></li>
 		<li class="article-links-item"><a href="https://web.archive.org/web/20*/${item.link}" target='_blank' rel='noopener norefferer nofollow'>[wbm]</a></li>
-        <li class="article-links-item"><a href="https://txtify.it/${item.link}" target='_blank' rel='noopener norefferer nofollow'>[txtfy]</a></li>
-		<li class="article-links-item"><a href="https://r3ad.deno.dev/${item.link}" target='_blank' rel='noopener norefferer nofollow'>[r3ad]</a></li>
       </ul>
     </header>
   </article>
